739ca29251938b6ab7658f58a81bad9de90503cf
oGuimaraes <otaviovgsr@gmail.com>
2020-09-22 22:23:43 -0300

    feat: brockerGui sketch

--- stock/gui_stock.py
+++ stock/gui_stock.py
@@ -1,3 +1,32 @@
 import PySimpleGUI as sg
+sg.theme('Default 1') 
 
-sg.Window(title="Stock/Bolsa de Valores", layout=[[]], margins=(100, 50)).read()
+# Layout
+transactions = []
+transacao1 = 'Compra, BRK1, 15, 16\n'
+transacao2 = 'Venda, BRK2, 15, 16\n' 
+transacao3 = 'Venda, BRK3, 15, 16\n'  
+transactions.append(transacao1)
+transactions.append(transacao2)
+transactions.append(transacao3)
+
+layout = [
+  [sg.Text('Stock Painel', size=(40, 0), font=('Helvetica', 12), justification='center')],
+  [sg.Text('Servidor'), sg.Input('localhost')],
+  [sg.Button('Abrir Negociações', size=(42,0))],
+  [sg.Output(size=(60,20))]
+]
+
+#Window
+window = sg.Window('Stock Painel', layout, margins=(20, 20))
+
+while True:
+  event, values = window.read()
+  #print(event)
+  #print(values)
+  if event == 'Abrir Negociações':
+    action = 'Venda, BRK3, 15, 16';
+    print('{}'.format(action))
+  if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
+    break
+window.close()
