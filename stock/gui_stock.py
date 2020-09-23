50aef512d35f332a61d88ce72b88363ba5977b19
oGuimaraes <otaviovgsr@gmail.com>
2020-09-22 23:27:15 -0300

    feat: back and front integration, emit_broker and stock_receive

--- stock/gui_stock.py
+++ stock/gui_stock.py
@@ -1,4 +1,9 @@
 import PySimpleGUI as sg
+from emit_stock import EmitStock
+from receive_stock import ReceiveStock
+import time
+
+
 sg.theme('Default 1') 
 
 # Layout
@@ -13,20 +18,24 @@ transactions.append(transacao3)
 layout = [
   [sg.Text('Stock Painel', size=(40, 0), font=('Helvetica', 12), justification='center')],
   [sg.Text('Servidor'), sg.Input('localhost')],
-  [sg.Button('Abrir Negociações', size=(42,0))],
   [sg.Output(size=(60,20))]
 ]
 
 #Window
 window = sg.Window('Stock Painel', layout, margins=(20, 20))
 
+receive_stock = ReceiveStock(host='localhost')
+receive_stock.start()
+
+
 while True:
   event, values = window.read()
   #print(event)
   #print(values)
-  if event == 'Abrir Negociações':
-    action = 'Venda, BRK3, 15, 16';
-    print('{}'.format(action))
   if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
     break
 window.close()
+
+
+
+
