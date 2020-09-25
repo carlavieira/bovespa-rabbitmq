ac20c39ef75119d74b3537f0e0a63159de6efb7a
Carla Vieira <carlaamvieira@gmail.com>
2020-09-24 23:41:03 -0300

    Fix file path

--- assets/assets_list.py
+++ assets/assets_list.py
@@ -4,12 +4,11 @@ class AssetsList():
 
     @staticmethod
     def get_assetslist():
-        file = open('bovespa_assets.csv', encoding='utf-8')
+        file = open('assets/bovespa_assets.csv', encoding='utf-8')
         lines = csv.reader(file, delimiter=";")
 
         assets = []
 
         for line in lines:
             assets.append(line[0])
-        print(assets)
         return assets
