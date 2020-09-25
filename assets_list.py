3740dcaf587ac2937ac4c5ef2094a9cac06ad40b
oGuimaraes <otaviovgsr@gmail.com>
2020-09-24 21:39:20 -0300

    gui_broker acess assets_list

--- assets_list.py
+++ assets_list.py
@@ -4,14 +4,12 @@ class AssetsList():
 
     @staticmethod
     def get_assetslist():
-
-        file = open('bovespa_assets.csv')
-
+        file = open('bovespa_assets.csv', encoding='utf-8')
         lines = csv.reader(file, delimiter=";")
 
         assets = []
 
         for line in lines:
             assets.append(line[0])
-
+        print(assets)
         return assets
--- gui_broker.py
+++ gui_broker.py
@@ -6,24 +6,17 @@ from assets_list import AssetsList
 
 sg.theme('Default 1') 
 
-
+# Get assets from assets_list
 assets = AssetsList.get_assetslist()
-print(assets)
 
 # Layout
-
 layout = [
   [sg.Text('Broker Panel', size=(40, 0), font=('Helvetica', 12), justification='center')],
   [sg.Text('Servidor'), sg.Input('localhost')],
-  [sg.Text('Broker'), sg.Input('BKR1')],
-  [sg.Text('Ativo'), sg.Combo([
-    'petr1', 'PETR4', 'petr2',
-    'ITUB5', 'BBDC4','BBAS3', 
-    'CIEL3', 'PEETR3', 'HYPE3', 
-    'VALE3','BBSE3', 'CTIP3', 
-    'GGBR4', 'FIBR3', 'RADL3'])],
-  [sg.Text('Quantidade'), sg.Input('', key='quantity')],
-  [sg.Text('Preço'), sg.Input('', key='price')],
+  [sg.Text('Broker'), sg.Combo(assets)],
+  [sg.Text('Ativo'), sg.Combo(assets)],
+  [sg.Text('Quantidade'), sg.Input('')],
+  [sg.Text('Preço'), sg.Input('')],
   [sg.Button('Compra', size=(20, 0)), sg.Button('Venda', size=(20, 0))],
   [sg.Button('Abrir Visualizador', size=(42,0))]
 ]
@@ -33,30 +26,11 @@ window = sg.Window('Broker Panel', layout, margins=(20, 20))
 
 while True:
   event, values = window.read()
-  """
-  submitValues = {
-    "ip": values[0],
-    "broker": values[1],
-    "active": values[2] ,
-    "quantity": values[3],
-    "price": values[4],
-    "event": event.lower()
-  }
-  print(routing_key, message)
-
-  print(submitValues)
-  """
-  if values[3] != 0 and values[4] != 0:
-    window.FindElement('price').Update('') 
-    window.FindElement('quantity').Update('')
-    if event.lower() == 'compra' or event.lower() == 'venda':
-      routing_key = event.lower() + '.' + values[2]
-      message = values[3] + '; ' + values[4] + '; ' + values[1]
-    
-      emit_broker = EmitBroker(host=values[0], routing_key=routing_key,      message=message)
-      emit_broker.start()
-      window.FindElement('price').Update('') 
-      window.FindElement('quantity').Update('')
+  if event.lower() == 'compra' or event.lower() == 'venda':
+    routing_key = event.lower() + '.' + values[2].lower()
+    message = values[3] + '; ' + values[4] + '; ' + values[1].lower()
+    print(message)
+    emit_broker = EmitBroker(host=values[0], routing_key=routing_key, message=message)
+    emit_broker.start() 
   if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
     break
-window.close()
